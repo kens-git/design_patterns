37f0842f35fa811562de151366e236dfff219ca5
Ken <kenmontgomery@protonmail.com>
2017-07-15 21:13:38 -0400

    Added State pattern

---
+++ state/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <memory>
+
+using std::cout;
+using std::endl;
+using std::unique_ptr;
+using std::make_unique;
+
+
+
+class GameState {
+    public:
+        virtual ~GameState() { }
+        virtual void update() = 0;
+        virtual void draw() = 0;
+};
+
+class MenuState : public GameState {
+    public:
+        MenuState() { cout << "Loading Menustate assets..." << endl; }
+        virtual ~MenuState() { cout << "Freeing Menustate assets..." << endl; }
+        void update() override { cout << "Menustate: Updating logic..." << endl; }
+        void draw() override { cout << "MenuState: Updating graphics..." << endl; }
+};
+
+class PlayState : public GameState {
+    public:
+        PlayState() { cout << "Loading Playstate assets..." << endl; }
+        virtual ~PlayState() { cout << "Freeing Playstate assets..." << endl; }
+        void update() override { cout << "Playstate: Updating logic..." << endl; }
+        void draw() override { cout << "Playstate: Updating graphics..." << endl; }
+};
+
+
+
+class AwesomeGame {
+    public:
+        AwesomeGame() : mCurrentState(make_unique<MenuState>()) { mainLoop(); }
+
+    private:
+        void mainLoop();
+        unique_ptr<GameState> mCurrentState;
+};
+
+void AwesomeGame::mainLoop() {
+    // There's no actual loop here, just a demonstration of the state changing
+
+    mCurrentState->update();
+    mCurrentState->draw();
+
+    // The user decided to play the game, change to PlayState
+    mCurrentState.reset();
+    mCurrentState = make_unique<PlayState>();
+
+    mCurrentState->update();
+    mCurrentState->draw();
+
+    cout << endl << "The user quits..." << endl;
+    mCurrentState.reset();
+}
+
+int main() {
+    AwesomeGame theGame;
+    cout << endl << "Thanks for playing!" << endl;
+    cout << "Your score was: 0" << endl;
+
+    return 0;
+}
---
+++ state/readme.md
@@ -0,0 +1,12 @@
+"The State Pattern allows an object to alter its behaviour when its internal state changes. The object will appear to change
+its class."
+
+The State Pattern was the first (and only) design pattern I every developed on my own*. I used it to keep track of the current
+state of a game I was working on, and it was the only reason a rookie like myself could start and *finish* my >10,000 line project.
+So, in a trip down memory lane, here's the State Pattern used to keep track of the current game state in an ineffably awesome
+game.
+
+
+
+
+*My programming career peaked on that day.
